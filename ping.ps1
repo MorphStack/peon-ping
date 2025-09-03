9f760654e11781534ba88733982e3d214d48f9ad
Brandon Tillman <brandon@brandontillman.com>
2025-09-02 18:56:00 -0700

    Updating config

--- ping.ps1
+++ ping.ps1
@@ -1,1 +1,16 @@
-Write-Host "PONG!"
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$Device,
+    [int]$Count = 4
+)
+
+function Test-Ping {
+    param (
+        [string]$Device,
+        [int]$Count
+    )
+
+    ping -n $Count $Device
+}
+
+Test-Ping -Device $env:PING_DEVICE -Count $env:PING_COUNT
